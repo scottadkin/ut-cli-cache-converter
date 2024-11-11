34b9a038c1b2f7adbd81af4282dbb00211a84aac
Scott Adkin <scottadkin001@gmail.com>
2024-11-11 16:03:20 +0000

    Fix to work with linux ./utpg

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
+const path = require('node:path'); 
 
 const bOverwrite = false;
+const cacheFolder = `Cache`;
+const cacheFile = `cache.ini`;
+
+const cacheIni = path.join(cacheFolder, cacheFile);
+
 
 class CacheCoverter{
 
@@ -8,12 +14,18 @@ class CacheCoverter{
 
         try{
 
-            console.log(`[------------------------------------------------------]`);
+            console.log(`[-------------------------------------------------------]`);
             console.log(`[-----CLI Unreal Tournament Cache Converter-------------]`);
-            console.log(`[------By Scott Adkin 6th December 2020----------------]`);
-            console.log(`[------------------------------------------------------]`);
+            console.log(`[------By Scott Adkin 6th December 2020-----------------]`);
+            console.log(`[------Linux Update 10th November 2024------------------]`);
+            console.log(`[-------------------------------------------------------]`);
 
             this.files = [];
+
+            //to keep in cache folder and cache.ini
+            this.failedFiles = [];
+
+            this.checkFolders();
 
             this.readIni();
 
@@ -23,12 +35,42 @@ class CacheCoverter{
         }catch(err){
             console.log(err);
         }
+    }
 
+    checkFolders(){
+
+        const targets = ["System",
+            "Sounds",
+            "Music",
+            "Textures",
+            "Maps"
+        ]
+
+        for(let i = 0; i < targets.length; i++){
+
+            const t = targets[i];
+
+            fs.stat(t, (err, stats) =>{
+    
+                if(err){
+
+                    console.log(`[Notice]: Failed to find folder ${t} (${err.code})`);  
+
+                    fs.mkdirSync(path.join(t));
+
+                    console.log(`[Notice]: Created folder ${t}`);  
+
+                    return;
+                }
+
+            });
+        }
+        
     }
 
     readIni(){
 
-        this.data = fs.readFileSync('Cache/Cache.ini').toString();
+        this.data = fs.readFileSync(cacheIni).toString();
 
         const reg = /^(.+)$/img;
         this.lines = this.data.match(reg);
@@ -53,8 +95,6 @@ class CacheCoverter{
     }
 
     convertFiles(){
-
-        let f = 0;
 
         const types = [
             "u",
@@ -83,16 +123,16 @@ class CacheCoverter{
         let pass = 0;
         let fail = 0;
         let dups = 0;
-
-        let typeIndex = 0;
-        let currentDest = 0;
-        let currentExt = 0;
-
-        let bCurrentExist = false;
+    
 
         for(let i = 0; i < this.files.length; i++){
 
-            f = this.files[i];
+            const f = this.files[i];
+
+            let bCurrentExist = false;
+            let typeIndex = 0;
+            let currentDest = 0;
+            let currentExt = 0;
 
             typeIndex = types.indexOf(f.ext.toLowerCase());
 
@@ -106,14 +146,16 @@ class CacheCoverter{
 
                 found[typeIndex].found++;
 
+                const currentFile = path.join(currentDest, `${f.name}.${currentExt}`);
+
                 try{
 
-                    fs.accessSync(`${currentDest}\\${f.name}.${currentExt}`,fs.constants.R_OK);   
+                    fs.accessSync(currentFile,fs.constants.R_OK);   
 
                     bCurrentExist = true;
 
                     if(!bOverwrite){
-                        console.log(`[Warning]: File ${currentDest}\\${f.name}.${currentExt} already Exists, not replacing.`);
+                        console.log(`[Warning]: File ${currentFile} already Exists, not replacing.`);
                     }
 
                     dups++;
@@ -124,15 +166,18 @@ class CacheCoverter{
                 }
 
 
+                const uxxFile = path.join(cacheFolder, `${f.guid}.uxx`);
+                
                 if(bOverwrite || !bCurrentExist){
 
-                    fs.renameSync(`Cache\\${f.guid}.uxx`,`${currentDest}\\${f.name}.${currentExt}`);
+                   
+                    fs.renameSync(uxxFile, currentFile);
                     found[typeIndex].pass++;
                     pass++;
-                    console.log(`[Pass]: File Cache\\${f.guid}.uxx converted to ${currentDest}\\${f.name}.${currentExt}.`);
+                    console.log(`[Pass]: File ${uxxFile} converted to ${currentFile}.`);
                 }else{
-                    fs.rmSync(`Cache\\${f.guid}.uxx`);
-                    console.log(`[Pass]: Cache\\${f.guid}.uxx Deleted.`);
+                    fs.rmSync(uxxFile);
+                    console.log(`[Pass]: ${uxxFile} Deleted.`);
                 }
 
                     
@@ -141,6 +186,8 @@ class CacheCoverter{
 
                 fail++;
                 found[typeIndex].fail++;
+
+                this.failedFiles.push(f.guid);
 
                 if(err.code === 'ENOENT'){
                     console.log(`[Warning]: The file ${f.guid}.uxx does not exist, skipping.`);
@@ -157,7 +204,7 @@ class CacheCoverter{
         console.log(`[Finished]: Found ${found[2].found} Music packages, ${found[2].pass} converted, ${found[2].dup} duplicates, and ${found[2].fail} failed.`);
         console.log(`[Finished]: Found ${found[3].found} Texture packages, ${found[3].pass} converted, ${found[3].dup} duplicates, and ${found[3].fail} failed.`);
         console.log(`[Finished]: Found ${found[4].found} Maps, ${found[4].pass} converted, ${found[4].dup} duplicates, and ${found[4].fail} failed.`);
-        console.log(`[Finished]: Failed conversions and usually caused by the file no longer existing in the Cache folder.`);
+        console.log(`[Finished]: Failed conversions are usually caused by the file no longer existing in the Cache folder.`);
         console.log(`[Finished]: Duplicate overwriting is set to ${bOverwrite}, to change this behaviour set bOverwrite to ${!bOverwrite} in app.js.`);
 
     }
@@ -167,8 +214,8 @@ class CacheCoverter{
 
         const data = `[Cache]`;
 
-        fs.writeFileSync(`Cache/Cache.ini`, data);
-        console.log(`[Finished]: Cache.ini now cleared.`);
+        fs.writeFileSync(cacheIni, data);
+        console.log(`[Finished]: cache.ini now cleared.`);
     }
 
     
